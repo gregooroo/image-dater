b253147aed74b50077921cc2acfa062c4f60d691
Mateusz <gregooroo@gmail.com>
2018-10-27 23:28:21 +0200

    Load files from directory

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 const program = require('commander');
+const imageDater = require('./lib');
 
 program
   .command('run <dir>')
   .description('Run a program')
-  .action((dir) => {
-    console.log(dir);
-  });
+  .action(dir => imageDater(dir));
 
 
 program.parse(process.argv);
