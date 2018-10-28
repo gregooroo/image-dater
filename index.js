de65de9c61e27465aecffa381816dc7dca8081a5
Mateusz <gregooroo@gmail.com>
2018-10-28 23:47:57 +0100

    Extract metadata from images

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const imageDater = require('./lib');
 program
   .command('run <dir>')
   .description('Run a program')
-  .action(dir => imageDater(dir));
+  .action((dir) => {
+    imageDater(dir)
+      .then(result => console.log(result))
+      .catch(err => console.error(err));
+  });
 
 
 program.parse(process.argv);
